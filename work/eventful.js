78c3b8ae63f9db9ed5fcf5cfc9f507530450a3a0
yurukyuruk <yurukyuruk@gmail.com>
2024-10-28 05:11:07 +0100

    add: css added

--- work/eventful.js
+++ work/eventful.js
@@ -48,3 +48,57 @@ nextImgBtn.addEventListener("click", () => {
     nextImgBtn.disabled = false;
   }, ANIMATION_TIME);
 });
+
+
+
+
+
+
+
+const finalPrototypeImagesContainer = document.querySelector('.final-prototype-photos');
+const finalPrototypeImages = Array.from(document.querySelectorAll('.final-prototype-photo'))
+const prevImageButton = document.querySelector('.prototype-prev-image');
+const nextImageButton = document.querySelector('.prototype-next-image');
+
+// Gap between elemnts need to be defined in PX
+const gapSizeBetweenImagesPixel = getComputedStyle(finalPrototypeImagesContainer).getPropertyValue('gap');
+const gapSizeBetweenImagesNumber = Number(gapSizeBetweenImagesPixel.slice(0, gapSizeBetweenImagesPixel.length - 2));
+const imageCount = finalPrototypeImagesContainer.length;
+const singleImageWidth = finalPrototypeImages[0].width;
+let currentImgIndex = 0;
+let prevImageIndex = 0;
+
+prevImageButton.addEventListener("click", () => {
+  prevImageButton.disabled = true;
+  prevImageIndex = currentImgIndex;
+  currentImgIndex = (currentImgIndex - 1 + imagesCount) % imagesCount;
+  finalPrototypeImagesContainer.style.transform = `translateX(-${singleImgWidth}px)`;
+  finalPrototypeImagesContainer.insertBefore(finalPrototypeImages[currentImgIndex], finalPrototypeImagesContainer.firstChild);
+
+  // This setTimeout is required as we need to wait once elemnt will be mounted in DOM
+  // To do not experience flashing images
+  setTimeout(() => {
+    finalPrototypeImagesContainer.style.transform = "";
+    finalPrototypeImagesContainer.classList.add("sliding-transition");
+  }, INSERT_PREV_IMAGE_DELAY);
+
+  setTimeout(() => {
+    finalPrototypeImagesContainer.classList.remove("sliding-transition");
+    prevImageButton.disabled = false;
+  }, ANIMATION_TIME - INSERT_PREV_IMAGE_DELAY);
+});
+
+nextImageButton.addEventListener("click", () => {
+  nextImageButton.disabled = true;
+  finalPrototypeImagesContainer.classList.add("sliding-transition");
+  prevImageIndex = currentImgIndex;
+  currentImgIndex = (currentImgIndex + 1) % imagesCount;
+  finalPrototypeImagesContainer.style.transform = `translateX(-${singleImgWidth + gapSizeBetweenImagesNum}px)`;
+
+  setTimeout(() => {
+    finalPrototypeImagesContainer.appendChild(finalPrototypeImages[prevImageIndex]);
+    finalPrototypeImagesContainer.classList.remove("sliding-transition");
+    finalPrototypeImagesContainer.style.transform = "";
+    nextImageButton.disabled = false;
+  }, ANIMATION_TIME);
+});
