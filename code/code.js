664ce83e8f5ab6c6639c5abf6fa7f7f5c91765b8
yurukyuruk <yurukyuruk@gmail.com>
2025-10-14 23:47:02 +0200

    feat: pages recreated by the content

---
+++ code/code.js
@@ -0,0 +1,29 @@
+const hamburgerButton = document.querySelector(".hamburger-checkbox");
+const navigation = document.querySelector(".mobile-header nav");
+const body = document.querySelector("body");
+
+if (hamburgerButton) {
+    hamburgerButton.addEventListener("change", () => {
+        if (navigation) {
+            navigation.classList.toggle("hidden", hamburgerButton.checked);
+            body.classList.toggle("body-hidden");
+        }
+    });
+}
+
+
+let lastScrollTop = 0;
+let header = document.querySelector("header");
+    window.addEventListener("scroll", function() {
+        const currentScroll = window.scrollY || document.documentElement.scrollTop;
+        if (currentScroll > lastScrollTop) {
+            // Scroll down
+            header.classList.add("header-hidden");
+        } else {
+            // Scroll up
+            header.classList.remove("header-hidden");
+        }
+        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+    }, false);    
+
+    
